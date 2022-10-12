c39c9641079d638a55450e132d757f5d98b83c35
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2022-10-12 15:08:04 +0300

    Initial Commit

---
+++ AwardsCollectionApp/AwardsCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionAppApp.swift
+//  AwardsCollectionApp
+//
+//  Created by Lera Savchenko on 12.10.22.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
